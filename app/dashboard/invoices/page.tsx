8ff6ff912fa2187cffc294567250180b672fb64f
MohGumaa <gomaam14@gmail.com>
2024-12-08 17:28:33 +0400

    added the function to get the invoice bill on in invoice table

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 const InvoicesPage = async (props: {
 	searchParams?: Promise<{
@@ -12,6 +13,11 @@ const InvoicesPage = async (props: {
 		page?: string;
 	}>;
 }) => {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+	const totalPages = await fetchInvoicesPages(query);
+
 	return (
 		<div className="w-full">
 			<div className="flex w-full items-center justify-between">
@@ -21,11 +27,11 @@ const InvoicesPage = async (props: {
 				<Search placeholder="Search invoices..." />
 				<CreateInvoice />
 			</div>
-			{/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+			 <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>
 			<div className="mt-5 flex w-full justify-center">
-				{/* <Pagination totalPages={totalPages} /> */}
+				<Pagination totalPages={totalPages} />
 			</div>
 		</div>
 	);
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,25 +1,26 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
 	const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
+	const handleSearch = useDebouncedCallback((term) => {
+		const params = new URLSearchParams(searchParams);
+		params.set('page', '1');
+		if (term) {
 			params.set('query', term);
 		} else {
 			params.delete('query');
 		}
+		replace(`${pathname}?${params.toString()}`);
+	}, 300);
 
-    replace(`${pathname}?${params.toString()}`);
-  }
-
-  return (
+	return (
 		<div className="relative flex flex-1 flex-shrink-0">
 			<label htmlFor="search" className="sr-only">
 				Search
